f503b77f40d3b0dbcda6e57a8032353074057aff
Fuad Ismayilbayli <fuad094@gmail.com>
2026-05-01 19:25:09 +0300

    feat: set up React Router with placeholder pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { SettingsProvider } from './context/SettingsContext'
+import HomePage from './pages/HomePage/HomePage'
+import SettingsPage from './pages/SettingsPage/SettingsPage'
+import ResultsPage from './pages/ResultsPage/ResultsPage'
+import GamePage from './pages/GamePage/GamePage'
+
 export default function App() {
-  return <div>vurma_cedveli</div>
+  return (
+    <SettingsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="/game" element={<GamePage />} />
+        </Routes>
+      </BrowserRouter>
+    </SettingsProvider>
+  )
 }
